ebc38f95711d9812af134f6d8e449f7e70ee3ff5
kimyounggoncom <younggon0925@gmail.com>
2025-07-22 10:09:16 +0900

    vercel 배포중

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // ▼▼▼ 바로 이 부분을 추가해주세요! ▼▼▼
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  // ▲▲▲ 여기까지 입니다 ▲▲▲
+
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
